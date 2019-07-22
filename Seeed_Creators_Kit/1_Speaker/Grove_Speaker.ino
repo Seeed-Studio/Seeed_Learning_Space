232b493f209992309bb8c3bdd205be19f58f7aa3
Anson He <ansonhe1997@gmail.com>
2019-07-22 14:10:13 +0800

    first add Grove speaker example code

---
+++ Seeed_Creators_Kit/1_Speaker/Grove_Speaker.ino
@@ -0,0 +1,33 @@
+/*macro definition of Speaker pin*/
+#define SPEAKER 3
+
+int BassTab[]={1911,1702,1516,1431,1275,1136,1012};//bass 1~7
+
+void setup()
+{
+    pinInit();
+}
+void loop()
+{
+        /*sound bass 1~7*/
+    for(int note_index=0;note_index<7;note_index++)
+    {
+        sound(note_index);
+        delay(500);
+    }
+}
+void pinInit()
+{
+    pinMode(SPEAKER,OUTPUT);
+    digitalWrite(SPEAKER,LOW);
+}
+void sound(uint8_t note_index)
+{
+    for(int i=0;i<100;i++)
+    {
+        digitalWrite(SPEAKER,HIGH);
+        delayMicroseconds(BassTab[note_index]);
+        digitalWrite(SPEAKER,LOW);
+        delayMicroseconds(BassTab[note_index]);
+    }
+}
