22c417074eb184db8a53e420d9534dcf650a10ff
Anson He <ansonhe1997@gmail.com>
2019-07-22 15:33:54 +0800

    Demo codes for Thumb Joystick

---
+++ Seeed_Creators_Kit/7.Thumb Joystick/1_thumb_joystick_serial.ino
@@ -0,0 +1,23 @@
+/*
+  Thumb Joystick demo v1.0
+  by:http://www.seeedstudio.com
+  connect the module to A0&A1 for using;
+*/
+
+void setup()
+{
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    int sensorValue1 = analogRead(A0);
+    int sensorValue2 = analogRead(A1);
+
+    Serial.print("The X and Y coordinate is:");
+    Serial.print(sensorValue1, DEC);
+    Serial.print(",");
+    Serial.println(sensorValue2, DEC);
+    Serial.println(" ");
+    delay(200);
+}
