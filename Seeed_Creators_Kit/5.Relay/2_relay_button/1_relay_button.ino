ce0396daf68992d4d2e7f0d07ff8a1416677f4b6
Anson He <ansonhe1997@gmail.com>
2019-07-22 15:29:03 +0800

    Demo codes for Relay

---
+++ Seeed_Creators_Kit/5.Relay/2_relay_button/1_relay_button.ino
@@ -0,0 +1,21 @@
+// Relay Control
+
+void setup()
+{
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
+  pinMode(4, OUTPUT);
+}
+
+void loop()
+{
+  if (digitalRead(2)==HIGH)
+  {
+    digitalWrite(4, HIGH);
+    delay(100);
+  }
+  if (digitalRead(3)==HIGH)
+  {
+    digitalWrite(4, LOW);
+  }
+}
