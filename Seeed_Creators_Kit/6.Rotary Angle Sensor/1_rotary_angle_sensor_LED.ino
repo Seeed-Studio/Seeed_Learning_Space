03e3c9b1aff5614e3d3522f9fdcf8f285ce32b59
Anson He <ansonhe1997@gmail.com>
2019-07-22 15:31:59 +0800

    Demo codes for Rotary Angle Sensor

---
+++ Seeed_Creators_Kit/6.Rotary Angle Sensor/1_rotary_angle_sensor_LED.ino
@@ -0,0 +1,30 @@
+/*macro definitions of Rotary angle sensor and LED pin*/
+
+#define ROTARY_ANGLE_SENSOR A0
+#define LED 3  //the Grove - LED is connected to PWM pin D3 of Arduino
+#define ADC_REF 5 //reference voltage of ADC is 5v.If the Vcc switch on the seeeduino
+                    //board switches to 3V3, the ADC_REF should be 3.3
+#define GROVE_VCC 5 //VCC of the grove interface is normally 5v
+#define FULL_ANGLE 300 //full value of the rotary angle is 300 degrees
+
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(ROTARY_ANGLE_SENSOR, INPUT);
+    pinMode(LED,OUTPUT);   
+}
+
+void loop()
+{   
+    float voltage;
+    int sensor_value = analogRead(ROTARY_ANGLE_SENSOR);
+    voltage = (float)sensor_value*ADC_REF/1023;
+    float degrees = (voltage*FULL_ANGLE)/GROVE_VCC;
+    Serial.println("The angle between the mark and the starting position:");
+    Serial.println(degrees);
+
+    int brightness;
+    brightness = map(degrees, 0, FULL_ANGLE, 0, 255);
+    analogWrite(LED,brightness);
+    delay(500);
+}
