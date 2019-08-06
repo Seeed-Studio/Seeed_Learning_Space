27759edd9a7393669c246cdb889c2f272901d1b3
Anson He <ansonhe1997@gmail.com>
2019-08-06 14:58:57 +0800

    Urgraded new demo code

--- Seeed_Creator_Kit/14.Four-Digit Display/NumberFlow/NumberFlow.ino
+++ Seeed_Creator_Kit/14.Four-Digit Display/NumberFlow/NumberFlow.ino
@@ -28,53 +28,37 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-//
 #include "TM1637.h"
-//Include 4-digit display library
-// Pins definitions for TM1637 and can be changed to other ports
-const int CLK = 6;
-const int DIO = 7;
+#define CLK 2//pins definitions for TM1637 and can be changed to other ports
+#define DIO 3
 TM1637 tm1637(CLK,DIO);
-
-float number = -1234.5;
-int mult = 1;
-
 void setup()
 {
-  Serial.begin(9600);
   tm1637.init();
   tm1637.set(BRIGHT_TYPICAL);//BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7;
-
-  float num = -123.45;
-
-  // Without specifying decimal pointt it displays int
-  tm1637.displayNum(num);    // -123
-  delay(2000);
-
-  // When numbers doesn't fit biggest digits are truncated
-  tm1637.displayNum(num, 1); // -234
-  delay(2000);
-  
-  // Colon light up as decimal point
-  tm1637.displayNum(num, 2);
-  delay(2000);
-  
-  // Note that adding 3 digits (-123450) makes number out of int range
-  tm1637.displayNum(num, 3);
-  delay(2000);
-
-  // Display negative numbers without sign
-  tm1637.displayNum(num, 0, false);
-  delay(2000);
 }
-
 void loop()
 {
-  tm1637.displayNum(number);
-  
-  number += 17.42 * mult;
-  if (abs(number) > 1500)
-    mult *= -1;
-  
-  delay(50);
+  int8_t NumTab[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};//0~9,A,b,C,d,E,F
+  int8_t ListDisp[4];
+  unsigned char i = 0;
+  unsigned char count = 0;
+  delay(150);
+  while(1)
+  {
+    i = count;
+    count ++;
+    if(count == sizeof(NumTab)) count = 0;
+    for(unsigned char BitSelect = 0;BitSelect < 4;BitSelect ++)
+    {
+      ListDisp[BitSelect] = NumTab[i];
+      i ++;
+      if(i == sizeof(NumTab)) i = 0;
+    }
+    tm1637.display(0,ListDisp[0]);
+    tm1637.display(1,ListDisp[1]);
+    tm1637.display(2,ListDisp[2]);
+    tm1637.display(3,ListDisp[3]);
+    delay(300);
+  }
 }
